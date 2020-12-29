661d4e7e4d7cf9de9c43d7eb7935472272c61292
yjchen0423 <chen.1571@osu.edu>
2020-12-29 00:54:28 -0800

    done

--- Ch02.py
+++ Ch02.py
@@ -75,3 +75,93 @@ Ch. 2.3.8
 print(len(letters))
 
 
+"""
+Ch. 2.3.9
+"""
+
+'split()'
+
+todos = 'get gloves,get mask,give cat vitamins,call ambulance'
+print(todos.split(','))
+
+
+"""
+Ch. 2.3.10
+"""
+
+'join()'
+
+crypto_list = ['Yeti', 'Bigfoot', 'Loch Ness Monster']
+crypto_string = ','.join(crypto_list)
+print(crypto_string)
+
+
+"""
+Ch 2.3.11
+"""
+
+'Practice'
+poem = '''
+All that doth flow we cannot liquid name
+Or else would fire and water be the same;
+But that is liquid which is moist and wet
+Fire that property can never get.
+Then 'tis not cold that doth the fire put out
+But 'tis the wet that makes it die, no doubt.
+'''
+
+print(poem[:13])
+print(len(poem))
+print(poem.startswith('All'))
+print(poem.endswith('That\'s all, folks!'))
+word = 'the'
+print(poem.find(word))
+print(poem.rfind(word))
+print(poem.count(word))
+print(poem.isalnum())
+
+
+"""
+Ch 2.3.12
+"""
+
+setup = 'a duck goes into a bar...'
+print(setup.strip('.'))
+print(setup.capitalize())
+print(setup.title())
+print(setup.upper())
+print(setup.lower())
+print(setup.swapcase())
+
+'string in a fix length'
+print(setup.center(30))
+print(setup.ljust(30))
+print(setup.rjust(30))
+
+
+"""
+Ch 2.3.13
+"""
+
+'replace()'
+
+print(setup.replace('duck', 'marmoset'))
+print(setup.replace('a ', 'a famous', 100))
+
+
+
+"""
+Ch 2.4
+"""
+
+'Exercies'
+
+print(60*60)
+seconds_per_hour = 60 * 60
+print(seconds_per_hour)
+
+seconds_per_day = 60 * 60 * 24
+print(seconds_per_day)
+
+print(seconds_per_day/seconds_per_hour)
+print(seconds_per_day//seconds_per_hour)
