55fab3d674637af60260a8e0a4b853283c883e80
yjchen0423 <chen.1571@osu.edu>
2021-01-08 10:07:05 -0800

    Ch 5_exercise_2021/01/07

---
+++ Ch05_exercise.py
@@ -0,0 +1,13 @@
+# 1.
+
+import zoo
+zoo.hours()
+
+# 2.
+import zoo as menagerie
+menagerie.hours()
+
+# 3.
+from zoo import hours
+hours()
+
---
+++ zoo.py
@@ -0,0 +1,6 @@
+"""Ch 5.7 Exercise"""
+
+# 1.
+def hours():
+    print('Open 9-5 daily')
+
