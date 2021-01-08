dcd9f6d2b6857980d0ccb907be9029febbb72de4
yjchen0423 <chen.1571@osu.edu>
2021-01-07 23:40:54 -0800

    Ch 5_2021/01/07

--- Ch05.py
+++ Ch05.py
@@ -27,3 +27,32 @@ print(periodic_table)
 
 'itertools'
 
+import itertools
+for item in itertools.chain([1, 2], ['a', 'b']):
+    print(item)
+
+'accumulate()'
+
+for item in itertools.accumulate([1, 2, 3, 4, 5]):
+    print(item)
+
+'use function as second argument in accumulate'
+def multiply(a, b):
+    return a * b
+
+for item in itertools.accumulate([1, 2, 3, 4, 5], multiply):
+    print(item)
+
+
+"""Ch 5.5.6"""
+
+'pprint()'
+
+from pprint import pprint
+quotes = [(1, 'aaaaaaaa'),
+          (2, 'bbbbbbbb'),
+          (3, 'cccccccc'),
+          (4, 'dddddddd')]
+
+print(quotes)
+pprint(quotes)
