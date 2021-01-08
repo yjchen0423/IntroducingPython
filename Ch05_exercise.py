a636f9a77eacaa55755b008fa448a4e275075d08
yjchen0423 <chen.1571@osu.edu>
2021-01-08 10:16:14 -0800

    Ch 5.7_exercise_2021/01/08

--- Ch05_exercise.py
+++ Ch05_exercise.py
@@ -11,3 +11,21 @@ menagerie.hours()
 from zoo import hours
 hours()
 
+# 4.
+from zoo import hours as info
+info()
+
+# 5.
+plain = {'a': 1, 'b': 2, 'c': 3}
+print(plain)
+
+# 6.
+from collections import OrderedDict
+fancy = OrderedDict(plain)
+print(fancy)
+
+# 7. *
+from collections import defaultdict
+dict_to_list = defaultdict(list)
+dict_to_list['a'].append('something for a')
+print(dict_to_list['a'])
