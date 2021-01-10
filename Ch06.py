4b1a43d85cf9417df04a0714a392b7ce3927d72d
yjchen0423 <chen.1571@osu.edu>
2021-01-10 15:28:17 -0800

    Ch 6.12_done_2021/01/10

--- Ch06.py
+++ Ch06.py
@@ -323,3 +323,39 @@ who_says(brook)
 
 """Ch. 6.12"""
 
+'magic method'
+
+
+'__eq__'
+'__repr__'
+class Word():
+    def __init__(self, text):
+        self.text = text
+    # def equals(self, word2):
+    def __eq__(self, word2):
+        return self.text.lower() == word2.text.lower()
+    def __str__(self):
+        return self.text
+    def __repr__(self):
+        return 'Word("' + self.text + '")'
+
+
+
+first = Word("ha")
+second = Word("HA")
+third = Word("eh")
+
+# print(first.equals(second))
+# print(first.equals(third))
+print(first == second)
+print(first == third)
+
+
+print(first)
+first
+
+
+
+"""Ch. 6.13"""
+
+
