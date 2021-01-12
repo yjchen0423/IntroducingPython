49df299a9d55e3214b902ecdd1ca8f0911531ac2
yjchen0423 <chen.1571@osu.edu>
2021-01-11 19:10:36 -0800

    Ch 7.1.3_2021/01/11

--- Ch07.py
+++ Ch07.py
@@ -14,3 +14,103 @@ unidoce_test('$')
 unidoce_test('\u20ac')
 unidoce_test('\u2603')
 
+
+snowman = '\u2603'
+print(len(snowman))
+ds = snowman.encode('utf-8')
+print(len(ds))
+
+place = 'caf\u00e9'
+print(place)
+
+place_bytes = place.encode('utf-8')
+print(place_bytes)
+
+
+"""Ch. 7.1.2"""
+
+'formatting'
+
+"""
+----old----
+%s string
+%d decimal integer
+%x hex integer
+%o octal integer
+%f decimal float
+%e exponential float
+%g decimal or exponential float
+%% a %
+"""
+
+print('%s' % 42)
+print('%d' % 42)
+print('%x' % 42)
+print('%o' % 42)
+
+print('%s' % 7.03)
+print('%f' % 7.03)
+print('%e' % 7.03)
+print('%g' % 7.03)
+
+print('%d%%' % 100)
+
+actor = 'Max'
+cat = 'Che'
+weight = 28
+
+print("My wife's favorite actor is %s" % actor)
+print("our cat's name is %s and her weight is %d pounds." % (cat, weight))
+
+n = 42
+f = 7.03
+s = 'string cheese'
+
+
+print("%d %f %s" % (n, f, s))
+print("%10d %10f %10s" % (n, f, s))
+print("%-10d %-10f %-10s" % (n, f, s))
+print("%10.4d %10.4f %10.4s" % (n, f, s))
+print("%.4d %.4f %.4s" % (n, f, s))
+print("%*.*d %*.*f %*.*s" % (10, 4, n, 10, 4, f, 10, 4, s))
+
+
+"""
+----new----
+
+"""
+
+print("{} {} {}".format(n, f, s))
+print("{2} {0} {1}".format(n, f, s))
+print("{n} {f} {s}".format(n = 42, f = 7.03, s = 'string cheese'))
+
+d = {'n':42, 'f':7.03, 's':'string cheese'}
+print("{0[n]} {0[f]} {0[s]} {1}".format(d, 'other'))
+
+print("{0:d} {1:f} {2:s}".format(n, f, s))
+
+print("{n:d} {f:f} {s:s}".format(n=42, f=7.03, s='string cheese'))
+
+print("{0:10d} {1:10f} {2:10s}".format(n, f, s))
+
+# right
+print("{0:>10d} {1:>10f} {2:>10s}".format(n, f, s))
+
+# left
+print("{0:<10d} {1:<10f} {2:<10s}".format(n, f, s))
+
+# center
+print("{0:^10d} {1:^10f} {2:^10s}".format(n, f, s))
+
+
+print("{0:^10d} {1:^10.2f} {2:^10.4s}".format(n, f, s))
+
+# there are total 20 chars and BIG SALE is in center
+print('{0:!^20s}'.format('BIG SALE'))
+
+
+"""Ch. 7.1.3"""
+
+'Regular Expression'
+
+
