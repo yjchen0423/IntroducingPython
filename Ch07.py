322615eb887f9c50fca4d55e38293d039a25ada3
yjchen0423 <chen.1571@osu.edu>
2021-01-18 09:12:40 -0800

    Ch 7.2_2021/01/18

--- Ch07.py
+++ Ch07.py
@@ -224,7 +224,31 @@ source = """I wish I may, I wish I might
             Have a dish of fish tonight."""
 
 print(re.findall('wish', source))
+print(re.findall('wish|fish', source))
+print(re.findall('^wish', source))
+print(re.findall('^I wish', source))
+print(re.findall('fish$', source))
+print(re.findall('fish tonight.$', source))
+print(re.findall('fish tonight\.$', source))
 
+print(re.findall('[wf]ish]', source))
+print(re.findall('[wsh]+]', source))
+print(re.findall('ght\W', source))
 
+print(re.findall('I (?=wish)', source))
+print(re.findall('(?<=I) wish', source))
+
+print(re.findall(r'\bfish', source))
+print(re.findall(r'fish', source))
+
+m = re.search(r'(. dish\b).*(\bfish)', source)
+print(m.group())
+print(m.groups())
+
+m = re.search(r'(?P<DISH>. dish\b).*(?P<FISH>\bfish)', source)
+print(m.group())
+print(m.groups())
+print(m.group('DISH'))
+print(m.group('FISH'))
 
 
