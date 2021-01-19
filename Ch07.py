db967b9741f5ebf49992ebb3404e5fccffb8355e
yjchen0423 <chen.1571@osu.edu>
2021-01-19 15:10:55 -0800

    Ch 7_done_2021/01/19

--- Ch07.py
+++ Ch07.py
@@ -238,7 +238,7 @@ print(re.findall('ght\W', source))
 print(re.findall('I (?=wish)', source))
 print(re.findall('(?<=I) wish', source))
 
-print(re.findall(r'\bfish', source))
+print(re.findall(r'\bfish', source)) # add r so that \b means a word boundary
 print(re.findall(r'fish', source))
 
 m = re.search(r'(. dish\b).*(\bfish)', source)
@@ -252,3 +252,125 @@ print(m.group('DISH'))
 print(m.group('FISH'))
 
 
+
+"""Ch. 7.2"""
+
+blist = [1, 2, 3, 255]
+the_bytes = bytes(blist)
+print(the_bytes)
+
+the_byte_array = bytearray(blist)
+print(the_byte_array)
+
+# the following code will be error as bytes are not changeable
+# the_bytes[1] = 127
+
+
+the_byte_array[1] = 127
+print(the_byte_array)
+
+
+the_bytes = bytes(range(0, 255))
+the_byte_array = bytearray(range(0, 255))
+
+print(the_bytes)
+print(the_byte_array)
+
+
+"""Ch. 7.2.2"""
+
+import struct
+
+valid_png_header = b'\x89PNG\r\n\x1a\n'
+
+
+
+
+"""Ch. 7.3"""
+
+# 1.
+mystery = '\U0001f4a9'
+print(mystery)
+
+import unicodedata
+mystery_name = unicodedata.name(mystery)
+print(mystery_name)
+
+# 2.
+pop_bytes = mystery.encode('utf-8')
+print(pop_bytes)
+
+# 3.
+pop_string = pop_bytes.decode('utf-8')
+print(pop_string)
+print(pop_string == mystery)
+
+# 4.
+print('My kitty cat likes %s, My kitty cat likes %s, My kitty cat fell on his %s And now thinks he\'s a %s'
+      % ('roast beef', 'ham', 'head', 'clam'))
+
+# 5.
+
+letter = """Dear {salutation} {name},
+            Thank you for your letter. We are sorry that our {product} {verbed} in your {room}. 
+            Please note that it should never be used in a {room}, especially near any {animals}.
+            Send us your receipt and {amount} for shipping and handling. 
+            We will send you another {product} that, in our tests, 
+            is {percent}% less likely to have {verbed}.
+            Thank you for your support.
+                Sincerely,
+                {spokesman}
+                {job_title}
+"""
+
+# 6.
+response = {'salutation': 'Mr.',
+            'name': 'Kevin',
+            'product': 'shampoo',
+            'verbed': 'ranned out',
+            'room': '1003',
+            'animals': 'new room',
+            'amount': '10',
+            'percent': '1',
+            'spokesman': 'Lisa',
+            'job_title': 'manager'}
+
+print(letter.format(**response))
+
+# 7.
+mammoth = """
+We have seen thee, queen of cheese,
+Lying quietly at your ease,
+Gently fanned by evening breeze,
+Thy fair form no flies dare seize.
+All gaily dressed soon you'll go
+To the great Provincial show,
+To be admired by many a beau
+In the city of Toronto.
+Cows numerous as a swarm of bees,
+Or as the leaves upon the trees,
+It did require to make thee please,
+And stand unrivalled, queen of cheese.
+May you not receive a scar as
+We have heard that Mr. Harris
+Intends to send you off as far as
+The great world's show at Paris.
+Of the youth beware of these,
+For some of them might rudely squeeze
+And bite your cheek, then songs or glees
+We could not sing, oh! queen of cheese.
+We'rt thou suspended from balloon,
+You'd cast a shade even at noon,
+Folks would think it was the moon
+About to fall and crush them soon.
+"""
+
+# 8.
+import re
+print(re.findall(r'\bc\w*', mammoth))
+
+# 9.
+print(re.findall(r'\bc\w{3}\b', mammoth))
+
+# 10.
+print(re.findall(r'\b\w*r\b', mammoth))
