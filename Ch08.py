dff447fff3bfa6dc0ba1ccfceb2d86f61076efef
yjchen0423 <chen.1571@osu.edu>
2021-01-29 15:50:28 -0800

    Ch. 8_done_01/29/2021

--- Ch08.py
+++ Ch08.py
@@ -297,3 +297,198 @@ print(data['details'])
 print(len(data['poems']))
 
 
+"""Ch. 8.2.7"""
+
+'configparser'
+
+import configparser
+cfg = configparser.ConfigParser()
+print(cfg.read('settings.cfg'))
+
+print(cfg['french'])
+print(cfg['french']['greeting'])
+print(cfg['files']['bin'])
+
+
+"""Ch. 8.4"""
+'SQL - Structured Query Language'
+
+
+'DDL - Data Definition Language'
+'DML - Data Manipulation Language'
+
+'DB - API'
+'connect()'
+'cursor()'
+'execute() & executemany()'
+'fetchone(), fetchmany(), fetchall()'
+
+import sqlite3
+conn = sqlite3.connect('enterprise.db')
+curs = conn.cursor()
+# curs.execute('''CREATE TABLE zoo
+# (critter VARCHAR(20) PRIMARY KEY,
+# count INT,
+# damages FLOAT)''')
+
+curs.execute('INSERT INTO zoo VALUES("duck", 5, 0.0)')
+curs.execute('INSERT INTO zoo VALUES("bear", 2, 1000.0)')
+
+'placeholder'
+ins = 'INSERT INTO zoo (critter, count, damages) VALUES(?, ?, ?)'
+curs.execute(ins, ('weasel', 1, 2000.0))
+
+curs.execute('SELECT * FROM zoo')
+rows = curs.fetchall()
+print(rows)
+
+curs.execute('SELECT * FROM zoo ORDER BY count')
+print(curs.fetchall())
+
+curs.execute('SELECT * FROM zoo ORDER BY count DESC')
+print(curs.fetchall())
+
+curs.execute('''SELECT * FROM zoo WHERE
+                damages = (SELECT MAX(damages) FROM zoo)''')
+print(curs.fetchall())
+
+curs.close()
+conn.close()
+
+
+"""Ch. 8.4.6"""
+'SQLAlchemy'
+
+import sqlalchemy as sa
+
+conn = sa.create_engine('sqlite://')
+
+conn.execute("""CREATE TABLE zoo2
+                (critter VARCHAR(20) PRIMARY KEY,
+                 count INT,
+                 damages FLOAT)""")
+
+ins = 'INSERT INTO zoo2 (critter, count, damages) VALUES (?, ?, ?)'
+conn.execute(ins, 'duck', 10, 0.0)
+conn.execute(ins, 'bear', 2, 1000.0)
+conn.execute(ins, 'weasel', 1, 2000.0)
+
+rows = conn.execute('SELECT * FROM zoo2')
+
+print(rows)
+
+for row in rows:
+    print(row)
+
+
+
+"""Ch. 8.5"""
+'NoSQL - not only SQL'
+
+
+"""Ch. 8.5.1"""
+'dbm family'
+
+import dbm
+db = dbm.open('definitions', 'c')
+db['mustard'] = 'yellow'
+db['ketchup'] = 'red'
+db['pesto'] = 'green'
+
+print(len(db))
+print(db['pesto'])
+
+db.close()
+
+db = dbm.open('definitions', 'r')
+print(db['mustard'])
+
+
+"""Ch. 8.7 Exercise"""
+
+# 1.
+test1 = "This is a test of the emergency test system"
+fout = open('test.txt', 'wt')
+fout.write(test1)
+fout.close()
+
+# 2.
+fin = open('test.txt', 'rt')
+test2 = fin.read()
+print(test1)
+print(test2)
+
+# 3.
+books = """authors,book
+J R R Tolkien,The Hobbit
+Lynne Truss,\"Eats, Shoots & Leaves\""""
+
+fout = open('books.csv', 'wt')
+fout.write(books)
+fout.close()
+
+# 4.
+import csv
+
+with open('books.csv', 'rt') as fin:
+    cin = csv.DictReader(fin)
+    books_csv = [row for row in cin]
+
+print(books_csv)
+
+# 5.
+books2 = """title,author,year
+The Weirdstone of Brisingamen,Alan Garner,1960
+Perdido Street Station,China Miéville,2000
+Thud!,Terry Pratchett,2005
+The Spellman Files,Lisa Lutz,2007
+Small Gods,Terry Pratchett,1992
+"""
+
+with open('books2.csv', 'wt') as fout:
+    fout.write(books2)
+
+# 6.
+import sqlite3
+conn = sqlite3.connect("books.db")
+curs = conn.cursor()
+# curs.execute('''CREATE TABLE book
+# (title VARCHAR(50) PRIMARY KEY,
+# author VARCHAR(50),
+# year INT)''')
+# curs.execute('''DROP TABLE book''')
+
+# 7.
+import sqlite3
+import csv
+ins = 'INSERT INTO book (title, author, year) VALUES(?, ?, ?)'
+with open('books2.csv', 'rt') as fin:
+    books = csv.DictReader(fin)
+    for book in books:
+        curs.execute(ins, (book['title'], book['author'], book['year']))
+
+rows = conn.execute('SELECT * FROM book')
+for row in rows:
+    print(row)
+
+
+# 8.
+print("------")
+titles = conn.execute('SELECT title FROM book ORDER BY title')
+for title in titles:
+    print(title[0])
+
+# 9.
+print("------")
+order_by_year = conn.execute('SELECT * FROM book ORDER BY year')
+for order in order_by_year:
+    print(*order, sep=',')
+
+# 10.
+print("10------")
+
+import sqlalchemy as sa
+conn = sa.create_engine('sqlite:///books.db')
+rows2 = conn.execute('SELECT title FROM book ORDER BY title')
+print(rows2)
+
