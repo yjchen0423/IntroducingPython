e222aacc04186494e2c8376cb951b789269ed161
yjchen0423 <chen.1571@osu.edu>
2021-02-27 09:30:50 -0800

    Ch. 9_02/27/2021

--- Ch09.py
+++ Ch09.py
@@ -7,3 +7,19 @@ url = 'http://www.example.com'
 conn = ur.urlopen(url)
 print(conn)
 print(conn.status)
+
+data = conn.read()
+print(data[:50])
+
+'''Convert the bytes to strings'''
+str_data = data.decode('utf8')
+print(str_data[:50])
+
+
+'''use requests'''
+import requests
+url = 'http://www.example.com'
+resp = requests.get(url)
+print(resp)
+print(resp.text[:50])
+
